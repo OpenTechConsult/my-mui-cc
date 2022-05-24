34897877b9de3f32644449792ef464ced448f3ba
OpenTech-Consult <opentech.consult@gmail.com>
2022-05-24 16:14:16 +0000

    Finishing the landing page
    - created the SearchAppBar component
    - created a data.json file which contains all the data we needed. In a real application it will be subtitute with a real database data
    - imported the SearchAppBAr inside the root App component
    - imported the data.json
    - inside the App.js iterate over the cities names and for each city name, iterate over city tour
    - inside the TourCard component, change static data by dynamic data form data.json
    
    closes #12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TourCard from './components/TourCard';
+import SearchAppBar from './components/SearchAppBar';
+import { Typography } from '@mui/material';
 import './App.css';
+
+import cities from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{marginY: 5}}>
+        {
+          cities.map((city) => (
+            <>
+            <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
+              {city.name}
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+            </>
+          ))
+        }
+        
       </Container>
     </div>
   );
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -33,31 +33,31 @@ const theme = createTheme({
     }
 });
 
-const TourCard = () => {
+const TourCard = ({tour}) => {
     return (
         <Grid item xs={3}>
             <ThemeProvider theme={theme}>
                 <Paper elevation={3}>
-                    <img src="https://images.unsplash.com/photo-1598402453861-4fbcbf6ced3b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674" alt=""
+                    <img src={tour.image} alt=""
                     className="img"/>
                     <Box paddingX={1}>
                         <Typography variant="subtitle1" component="h2">
-                            Immerse into the fall
+                            {tour.name}
                         </Typography>
                         <Box sx={{display: "flex", alignItems: "center"}}>
                             <AccessTimeIcon sx={{ width: 12.5 }} />
                             <Typography variant="body2" component="p" marginLeft={0.5}>
-                                5 hours
+                                {tour.duration} hours
                             </Typography>
                         </Box>
                         <Box sx={{ display: "flex", alignItems: "center"}} marginTop={3}>
-                            <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
-                            <Typography variant="body2" component="p" marginLeft={0.5}>4.5</Typography>
-                            <Typography variant="body2" component="p" marginLeft={1.5}>(655 reviews)</Typography>
+                            <Rating name="read-only" value={tour.rating} readOnly precision={0.5} size="small"/>
+                            <Typography variant="body2" component="p" marginLeft={0.5}>{tour.rating}</Typography>
+                            <Typography variant="body2" component="p" marginLeft={1.5}>({tour.numberOfReviews} reviews)</Typography>
                         </Box>
                         <Box>
                             <Typography variant="h6" component="h3" marginTop={0}>
-                                From C $465
+                                From C ${tour.price}
                             </Typography>
                         </Box>
                     </Box>
